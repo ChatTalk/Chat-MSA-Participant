b7835bd71a997b6c32b3e43b54c66246c700ccc0
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-04 20:32:24 +0900

    initial setting

---
+++ src/main/java/com/example/chatserverparticipant/ChatServerParticipantApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatserverparticipant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerParticipantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerParticipantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatserverparticipant/ChatServerParticipantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatserverparticipant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerParticipantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
