815e835154e5dd51000aacc144125e2e6f34736b
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-06 00:31:36 +0900

    fix: 어노테이션 할당 오류 수정

--- src/test/java/com/example/chatserverparticipant/ChatServerParticipantApplicationTests.java
+++ src/test/java/com/example/chatserverparticipant/ChatServerParticipantApplicationTests.java
@@ -2,9 +2,7 @@ package com.example.chatserverparticipant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootTest
 class ChatServerParticipantApplicationTests {
 
