9889c7ccccfa0a4402b89052708c13ef7ba4fb30
kimD0ngjun <chickentasty0112@gmail.com>
2024-10-09 02:56:03 +0900

    error: 분산 락 적용, 개념 오류 확인, 대기열 구현으로 선회

--- src/main/java/com/example/chatserverparticipant/global/redis/RedisMessageListenerService.java
+++ src/main/java/com/example/chatserverparticipant/global/redis/RedisMessageListenerService.java
@@ -1,6 +1,7 @@
 package com.example.chatserverparticipant.global.redis;
 
 import com.example.chatserverparticipant.domain.dto.UserReadDTO;
+import com.example.chatserverparticipant.global.facade.DistributedLockFacade;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,7 +19,7 @@ import java.util.List;
 @Service
 public class RedisMessageListenerService implements MessageListener {
 
-//    private final SseEmitterRepository sseEmitterRepository;
+    private final DistributedLockFacade distributedLockFacade;
     private final SimpMessageSendingOperations messagingTemplate;
 
     @Override
@@ -36,24 +37,13 @@ public class RedisMessageListenerService implements MessageListener {
             List<UserReadDTO> dto = new ObjectMapper().readValue(body, new TypeReference<List<UserReadDTO>>() {});
             log.info("파싱: {}", dto.stream().map(e -> e.getEmail() + ": " + e.getIsRead()).toList());
 
-            messagingTemplate.convertAndSend("/sub/participant/" + id, dto);
-//            // 해당 채팅방의 SSE Emitter 찾기
-//            Optional<SseEmitter> optionalEmitter = sseEmitterRepository.findById(channel);
-//            optionalEmitter.ifPresent(emitter -> {
-//                try {
-//                    // 이벤트 타입 및 ID를 명시적으로 설정
-//                    emitter.send(SseEmitter.event()
-//                            .id(channel.replaceFirst("chat_", "")) // 이벤트 ID 설정
-////                            .name("onMessage") // 이벤트 타입 설정(리액트랑 동일하게 맞춰야 함)
-//                            .data(dto)); // SSE Emitter로 데이터 전송
-//                } catch (IOException e) {
-//                    log.error("SSE 전송 오류: {}", e.getMessage());
-////                    sseEmitterRepository.deleteById(channel); // 오류 발생 시 emitter 제거
-//                } catch (IllegalStateException e) {
-//                    log.error("SSE 상태 오류: {}", e.getMessage());
-//                    sseEmitterRepository.deleteById(channel); // 상태 오류 발생 시 제거
-//                }
-//            });
+            if (distributedLockFacade.tryLock(id, 5, 60)) {
+                messagingTemplate.convertAndSend("/sub/participant/" + id, dto);
+                log.info("메시지 전송 완료: {}", id);
+                distributedLockFacade.unlock(id);
+            } else {
+                log.warn("해당 구독 경로에 락이 설정되지 않아 메시지 전송을 중단: {}", id);
+            }
         } catch (JsonProcessingException e) {
             log.error("메시지 파싱 오류: {}", e.getMessage());
         }
